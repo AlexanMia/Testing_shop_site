fa67659dab76e1f70b005e74e0a66d59bcf5767f
Inna <idinna91@mail.ru>
2022-08-14 17:27:03 +0300

    update constants

---
+++ util/Constants.py
@@ -0,0 +1,7 @@
+class Constants:
+    link_site = 'http://automationpractice.com/index.php'
+    EMAIL = 'felahek839@ukgent.com'
+    PASSWORD = 'page71191'
+    NAME_USER = 'Jaide Saford'
+    CHECKING_COLOR_BLACK = 'black'
+    SIZE_M = 'M'
